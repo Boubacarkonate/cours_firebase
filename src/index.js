d9c728f3dc203aec0a580288ae72a2fc68ae8926
Bouba <boubacar.konate@outlook.fr>
2024-02-22 20:48:17 +0100

    requêtes firestore

--- src/index.js
+++ src/index.js
@@ -12,7 +12,11 @@ import {
     setDoc,
     doc,
     deleteDoc,
-    updateDoc
+    updateDoc,
+    query,
+    where,
+    orderBy,
+    limit
 } from "firebase/firestore";
 
 
@@ -141,6 +145,243 @@ updateCityForm.addEventListener('submit', (event) => {
 })
 
 
+                            /**************************************************************
+                             *            LES REQUETES FIRESTORE : simple et compléxe
+                             *************************************************************/
+
+//Ajouter un dataset dans BK collection "Villes"
+Promise.all([
+    setDoc(doc(cities, "KIN"), {
+        pays: "Rd Congo",
+        ville: "Kinshasa",
+        capitale: true,
+        dateDajout: new Date("Jul 1, 2022"),
+        population: 15000000,
+        communes: [
+        "Gombe",
+        "Bandale",
+        "Kinshasa",
+        "LingwaBK",
+        "Limete",
+        "Ngaliema",
+        ],
+    }),
+    setDoc(doc(cities, "BK"), {
+        pays: "Rd Congo",
+        ville: "Bukavu",
+        capitale: false,
+        dateDajout: new Date("Jul 6, 2022"),
+        population: 2000000,
+        communes: ["Ibanda", "Kadutu", "Bagira"],
+    }),
+    setDoc(doc(cities, "DEGO"), {
+        pays: "Rd Congo",
+        ville: "Goma",
+        capitale: false,
+        dateDajout: new Date("Jul 9, 2022"),
+        population: 1000000,
+        communes: ["Goma", "Karisimbi"],
+    }),
+    setDoc(doc(cities, "BJ"), {
+        pays: "Burundi",
+        ville: "Bujumbura",
+        capitale: false,
+        dateDajout: new Date("Jul 15, 2022"),
+        population: 1500000,
+        communes: ["Ntahangwa", "Mukazi", "Muha"],
+    }),
+    setDoc(doc(cities, "GTG"), {
+        pays: "Burundi",
+        ville: "Gitega",
+        capitale: true,
+        dateDajout: new Date("Jul 17, 2022"),
+        population: 130000,
+        communes: ["Magara", "Nyamugari", "Rutonde"],
+    }),
+    setDoc(doc(cities, "KGL"), {
+        pays: "Rwanda",
+        ville: "Kigali",
+        capitale: true,
+        dateDajout: new Date("Jul 28, 2022"),
+        population: 1500000,
+        communes: ["Gasabo", "Kicukiro", "Nyarugenge"],
+    }),
+    setDoc(doc(cities, "GSG"), {
+        pays: "Rwanda",
+        ville: "Gisenyi",
+        capitale: false,
+        dateDajout: new Date("Jul 18, 2022"),
+        population: 160000,
+        communes: ["Kibuye", "Cyangugu"],
+    }),
+    setDoc(doc(cities, "NBO"), {
+        pays: "Kenya",
+        ville: "Nairobi",
+        capitale: true,
+        dateDajout: new Date("Jul 10, 2022"),
+        population: 4000000,
+        communes: [
+        "WestBKnds",
+        "Dagoretti",
+        "BKngata",
+        "Kamukunji",
+        "Embakasi",
+        "Njiru",
+        "Kakadara",
+        ],
+    }),
+    setDoc(doc(cities, "MBS"), {
+        pays: "Kenya",
+        ville: "Mombasa",
+        capitale: false,
+        dateDajout: new Date("Jul 3, 2022"),
+        population: 120800,
+        communes: ["Changwaniwe", "Kisauni", "Koni", "Lokini"],
+    }),
+    ])
+    .then(() => console.log("Données 'Villes' ajoutées avec succès"))
+    .catch((error) => console.log(error.message));
 
 
-                        
+//Ajouter un dataset dans BK sous-collection "habitants"
+Promise.all([
+    addDoc(collection(cities, "KIN", "habitants"), {
+      noms: "Patrick Bashizi",
+      age: "35 ans",
+      sexe: "M",
+    }),
+    addDoc(collection(cities, "KIN", "habitants"), {
+      noms: "Odette Kavira",
+      age: "32 ans",
+      sexe: "F",
+    }),
+    addDoc(collection(cities, "BK", "habitants"), {
+      noms: "Alain Cisirika",
+      age: "27 ans",
+      sexe: "M",
+    }),
+    addDoc(collection(cities, "BK", "habitants"), {
+      noms: "Josephine Romana",
+      age: "22 ans",
+      sexe: "F",
+    }),
+    addDoc(collection(cities, "DEGO", "habitants"), {
+      noms: "Lens Mutombo",
+      age: "30 ans",
+      sexe: "M",
+    }),
+    addDoc(collection(cities, "DEGO", "habitants"), {
+      noms: "Josephine Ndeze",
+      age: "23 ans",
+      sexe: "F",
+    }),
+    addDoc(collection(cities, "BJ", "habitants"), {
+      noms: "Jean Lionel",
+      age: "28 ans",
+      sexe: "M",
+    }),
+    addDoc(collection(cities, "GTG", "habitants"), {
+      noms: "Chouella Kayonga",
+      age: "23 ans",
+      sexe: "F",
+    }),
+    addDoc(collection(cities, "KGL", "habitants"), {
+      noms: "Cynthia React",
+      age: "24 ans",
+      sexe: "F",
+    }),
+    addDoc(collection(cities, "GSG", "habitants"), {
+      noms: "Esther Android",
+      age: "26 ans",
+      sexe: "M",
+    }),
+    addDoc(collection(cities, "NBO", "habitants"), {
+      noms: "Tabitha CrowSource",
+      age: "29 ans",
+      sexe: "F",
+    }),
+    addDoc(collection(cities, "MBS", "habitants"), {
+      noms: "Wayne Angular",
+      age: "30 ans",
+      sexe: "M",
+    }),
+  ])
+    .then(() => console.log("Données 'habitants' ajoutées avec succès"))
+    .catch((error) => console.log(error.message));
+
+
+    //////////////////// LES REQUETES SIMPLES //////////////////////
+
+//1. Création d'une requête pour récupérer les documents de la collection "Villes" où le champ "pays" est égal à 'RD Congo'
+const q1 = query(cities, where("pays", '==', 'Rd Congo'));
+
+//2. Récuperer toutes les villes sauf celles de la RDC
+const q2 = query(cities, where("pays", "!=", "Rd Congo"));
+
+//3 Récuperer seulement les villes de la RD Congo et celles du Rwanda
+const q3 = query(cities, where("pays", "in", ["Rd Congo", "Rwanda"]));
+
+//4. Récuperer toutes les villes sauf 'Bujumbura', 'Gisenyi', 'Goma'
+const q4 = query(
+    cities,
+    where("ville", "not-in", ["Bujumbura", "Gisenyi", "Goma"])
+  );
+
+//5. Récuperer les villes dont la population est superieure à 1M
+const q5 = query(cities, where("population", ">", 1000000));
+
+//6. Récuperer toutes les villes ajoutées entre le 10 et 30 juillet 2022
+// et Arrangez-les selon l'odre decroissant
+const q6 = query(
+    cities,
+    where(
+      "dateDajout",
+      ">",
+      new Date("Jul 10, 2022"),
+      where("dateDajout", "<", new Date("Jul 30, 2022")),
+      orderBy("dateDajout", "desc")
+    )
+  );
+
+//7. Récuperer la ville avec comme commune 'Nyarugege'
+const q7 = query(cities, where("communes", "array-contains", "Nyarugenge"));
+
+
+//8. Récuperer les villes avec comme commune 'Nyarugege', 'Bandale', 'Cyangugu', 'Ibanda'
+const q8 = query(
+    cities,
+    where("communes", "array-contains-any", [
+      "Nyarugenge",
+      "Bandale",
+      "Cyangugu",
+      "Ibanda",
+    ])
+  );
+
+//9. Récuperer les 3 dernieres villes recement ajoutées
+const q9 = query(cities, orderBy("dateDajout", "desc"), limit(3));
+
+
+// Écoute des modifications dans les documents qui correspondent à la requête q1
+onSnapshot(q8, (snapshot) => {
+    let recuperer_city_inArray = []; // Initialisation d'un tableau pour stocker les données des villes récupérées
+    // Parcours de chaque document dans le snapshot
+    snapshot.docs.forEach((doc) => {
+        // Ajout des données de chaque document dans le tableau recuperer_city_inArray
+        recuperer_city_inArray.push({ ...doc.data(), id: doc.id }); // On utilise spread operator pour obtenir les données du document et on ajoute l'ID du document
+    });
+
+    // Affichage des données des villes dans la console
+    // console.log('q1. requête pour obtenir toutes les villes de la RD Congo : ', recuperer_city_inArray);
+    // console.log('q2. requête pour obtenir toutes les villes exceptées celles de la RD Congo : ', recuperer_city_inArray);
+    // console.log('q3. requête pour obtenir toutes les villes  de la RD Congo et du Rwanda : ', recuperer_city_inArray);
+    // console.log('q4. requête pour obtenir récuperer toutes les villes sauf Bujumbura, Gisenyi, Goma  : ', recuperer_city_inArray);
+    // console.log('q5. requête pour récuperer les villes dont la population est superieure à 1M  : ', recuperer_city_inArray);
+    // console.log('q6. Récuperer toutes les villes ajoutées entre le 10 et 30 juillet 2022 dans un ordre décroissant : ', recuperer_city_inArray);
+    // console.log('q7. Récuperer la ville avec comme commune Nyarugege : ', recuperer_city_inArray);
+    // console.log('q8. Récuperer les villes avec comme commune Nyarugege, Bandale, Cyangugu, Ibanda : ', recuperer_city_inArray);
+    console.log('q9. Récuperer les 3 dernieres villes recement ajoutées : ', recuperer_city_inArray);
+
+});
+    
+
